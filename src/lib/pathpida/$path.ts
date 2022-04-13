f31bae3c5422c789f0ce3f8edc328d0ee2460cf6
hisho <hisho.web@gmail.com>
2022-04-14 02:34:18 +0900

    ✨ generate

--- src/lib/pathpida/$path.ts
+++ src/lib/pathpida/$path.ts
@@ -1,4 +1,10 @@
 export const pagesPath = {
+  "card": {
+    _id: (id: string | number) => ({
+      $url: (url?: { hash?: string }) => ({ pathname: '/card/[id]' as const, query: { id }, hash: url?.hash })
+    }),
+    $url: (url?: { hash?: string }) => ({ pathname: '/card' as const, hash: url?.hash })
+  },
   $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
 }
 
